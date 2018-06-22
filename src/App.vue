a3c0c58198c7a986805bbd89d188baf88cf67019
Eric Berry <eric@berry.sh>
2018-06-22 16:19:18 -0600

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <CodeFund
+      propertyId="51b2fb2d-92c0-4c1a-b60b-825eb76e2a2f"
+      template="default"
+      theme="light"
+    />
+  </div>
+</template>
+
+<script>
+import CodeFund from "./components/CodeFund";
+
+export default {
+  name: "App",
+  components: {
+    CodeFund
+  }
+};
+</script>
---
+++ src/components/CodeFund.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="codefund">
+    <div ref="codefund" id="codefund_ad"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CodeFund",
+  mounted() {
+    const script = document.createElement("script");
+    script.setAttribute("type", "text/javascript");
+    script.setAttribute(
+      "src",
+      `https://codefund.io/scripts/${this.propertyId}/embed.js?template=${
+        this.template
+      }&theme=${this.theme}`
+    );
+    this.$refs.codefund.appendChild(script);
+  },
+  props: {
+    propertyId: {
+      type: String,
+      required: true,
+      validator: value => {
+        const r = RegExp(
+          "^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$",
+          "i"
+        );
+        return r.test(value);
+      }
+    },
+    template: {
+      type: String,
+      default: "default"
+    },
+    theme: {
+      type: String,
+      default: "light"
+    }
+  }
+};
+</script>
